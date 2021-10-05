e32afcd451c598aafedd3821edbcd9bc19e63923
mesu-ai <mesu.s.ss@gmail.com>
2021-10-05 10:34:09 +0600

    header brand name + favicon add

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
+import brandlogo from './brandlogo.png';
 
 const Header = (props) => {
   console.log(props);
     return (
+      
   
 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
   <Container>
         <Navbar.Brand>
-                        {/* <img
+                        <img
                         alt=""
-                        src="/logo.svg"
-                        width="30"
-                        height="30"
+                        src={brandlogo}
+                        width="60"
+                        height="40"
                         className="d-inline-block align-top"
-                        />{' '} */}
-                        E-Learning
+                        />{' '}
+                       <span className="text-danger fs-3 brandtext">E-Learning</span>
         </Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
@@ -25,9 +27,9 @@ const Header = (props) => {
     <Nav className="ms-auto nav-Container">
     
         <NavLink activeClassName='active' to="/home">Home</NavLink>
-        <NavLink to="/services">Services</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contract">Contract</NavLink>
+        <NavLink activeClassName to="/services">Services</NavLink>
+        <NavLink activeClassName to="/about">About</NavLink>
+        <NavLink activeClassName to="/contract">Contract</NavLink>
         {/* <NavLink to=""><i className="fas fa-cart-plus fa-lg"></i>{props.quantity}</NavLink> */}
     </Nav>
   </Navbar.Collapse>
